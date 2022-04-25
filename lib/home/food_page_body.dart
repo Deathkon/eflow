56f2eca69ec36d79a227abc72e8135d61aa52d6c
Deathkon <shyakasmith@gmail.com>
2022-04-25 17:44:27 +0200

    improvement on page view builder

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:food_delivery/utils/colors.dart';
+import 'package:food_delivery/widgets/big_text.dart';
+import 'package:food_delivery/widgets/icon_and_text_widget.dart';
+import 'package:food_delivery/widgets/small_text.dart';
 
 class FoodPageBody extends StatefulWidget {
   const FoodPageBody({Key? key}) : super(key: key);
@@ -10,6 +14,22 @@ class FoodPageBody extends StatefulWidget {
 
 class _FoodPageBodyState extends State<FoodPageBody> {
   PageController pageController = PageController(viewportFraction: 0.85);
+  var _currentPageValue=0.0;
+  double _scaleFactor = 0.8;
+  @override
+  void initState(){
+    super.initState();
+    pageController.addListener(() {
+      setState(() {
+        _currentPageValue = pageController.page!;
+
+      });
+    });
+  }
+  @override
+  void dispose(){
+    pageController.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -23,11 +43,15 @@ class _FoodPageBodyState extends State<FoodPageBody> {
     );
   }
   Widget _buildPageItem(int index){
+    Matrix4 matrix4 = new Matrix4.identity();
+    if(index == _currentPageValue.floor()){
+      var currScale = 1-(_currentPageValue-index)*(1-_scaleFactor);
+    }
     return Stack(
       children: [
         Container(
       height: 220,
-      margin: EdgeInsets.only(left: 5, right: 5),
+      margin: EdgeInsets.only(left: 10, right: 10),
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(30),
           color: index.isEven?Color(0xFF69c5df):Color(0xFF9294cc),
@@ -42,12 +66,59 @@ class _FoodPageBodyState extends State<FoodPageBody> {
         Align(
           alignment: Alignment.bottomCenter,
           child: Container(
-            height: 140,
-            margin: EdgeInsets.only(left: 30, right: 30, bottom: 15),
+            height: 120,
+            margin: EdgeInsets.only(left: 20, right: 20, bottom: 30),
             decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(30),
                 color: Colors.white,
 
+            ),
+            child: Container(
+              padding: EdgeInsets.only(top: 15, left: 15, right: 15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+
+                children: [
+                  BigText(text: "Chinese Side"),
+                  SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Wrap(
+                        children: List.generate(5, (index) { return
+                                  Icon(
+                                    Icons.star,
+                                    color: AppColors.mainColor,
+                                    size: 15,
+                                  );
+                                }),
+                      ),
+                      SizedBox(width: 10),
+                      SmallText(text: "4.5"),
+                      SizedBox(width: 10),
+                      SmallText(text: "1287"),
+                      SizedBox(width: 10),
+                      SmallText(text: "Comments")
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    children: [
+                      IconAndTextWidget(
+                          icon: Icons.circle_sharp,
+                          text: "Normal",
+                          iconColor: AppColors.iconColor1),
+                      IconAndTextWidget(
+                          icon: Icons.location_on,
+                          text: "2.3km",
+                          iconColor: AppColors.mainColor),
+                      IconAndTextWidget(
+                          icon: Icons.access_time_rounded,
+                          text: "40 hour",
+                          iconColor: AppColors.iconColor2)
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         )
